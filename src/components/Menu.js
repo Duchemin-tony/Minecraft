5ab68a3a7ea24ed4c9a59e764807574b9d3b482e
Tony <duchemin.tony.dev@gmail.com>
2023-01-13 12:10:25 +0100

    Correction

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ import { useStore } from "../hooks/useStore"
 
 
 export const Menu = () => {
-    const [saveWorld, resetWorld] = useStore((state) => [state.saveWordl, state.resetWorld])
+    const [saveWorld, resetWorld] = useStore((state) => [state.saveWorld, state.resetWorld])
 
     return (
         <div className="menu absolute">
